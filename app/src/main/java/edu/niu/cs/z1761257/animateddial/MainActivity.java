d6a99473142a1f9e616bfc482decc7f98c8f93ac
Pravin Kandala <pravinkandala@hotmail.com>
2016-04-26 09:13:32 -0500

    working

--- app/src/main/java/edu/niu/cs/z1761257/animateddial/MainActivity.java
+++ app/src/main/java/edu/niu/cs/z1761257/animateddial/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.niu.cs.z1761257.animateddial;
 
 import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -14,5 +16,46 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //create DialView object
+        dialView = new DialView(this);
+        setContentView(dialView);
+
+        animationThread = new Thread(runningAnimation);
+        animationThread.start();
     }//end of onCreate
-}
+
+    private Runnable runningAnimation = new Runnable() {
+
+        private static final int DELAY = 200;
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(DELAY);
+                    threadHandler.sendEmptyMessage(0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }//end of while
+        }//end of run
+    };//end of runningAnimation runnable
+
+    private Handler threadHandler = new Handler(){
+                @Override
+                public void handleMessage(Message msg) {
+                  //  super.handleMessage(msg);
+                dialView.update();
+                }
+            };
+
+            @Override
+            protected void onPause()
+            {
+                super.onPause();
+                threadHandler.removeCallbacks(runningAnimation);
+
+            }
+
+
+}//end of MainActivity
